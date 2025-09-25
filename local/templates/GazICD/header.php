210a5992486829e453e1dc550ecdc685bc78dc82
Марк Андреев <andre3v.mark@yandex.ru>
2025-09-25 10:04:43 +0300

    Создать сайт Телефонный справочник ГазпромИнформ

--- local/templates/GazICD/header.php
+++ local/templates/GazICD/header.php
@@ -6,11 +6,22 @@ use Bitrix\Main\Page\Asset;
 <head>
     <?php $APPLICATION->ShowHead(); ?>
     <title><?php $APPLICATION->ShowTitle(); ?></title>
+    <?php $APPLICATION->ShowPanel(); ?>
 
-
-
+    <?php
+    Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/css/style.css");
+    ?>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-
 </head>
 <body>
-<?php $APPLICATION->ShowPanel(); ?>
+<div class="custom-header-block">
+    <div class="logo">
+        <img src="<?=SITE_TEMPLATE_PATH?>/img/gazprom-inform 50.svg" alt="Газпром Информ">
+    </div>
+    <nav class="nav">
+        <a href="#">Ссылка 1</a>
+        <a href="#">Ссылка 2</a>
+        <a href="#">Ссылка 3</a>
+    </nav>
+</div>
